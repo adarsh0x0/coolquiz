c88f3ac57bc9c2c27b353d9d6e15a16a240544e9
Adarsh Thapa <realadarshthapa@gmail.com>
2020-12-31 10:24:18 +0545

    refactored to clean state

--- Question.java
+++ Question.java
@@ -8,15 +8,15 @@ public class Question {
 
 	static int nQuestions = 0;
 	static int nCorrect = 0;
-	
+
 	String ask() {
 		question.setVisible(true);
 		return question.answer;
 	}
-	
+
 	Question(String question) {
 		this.question = new QuestionDialog();
-		this.question.setLayout(new GridLayout(0,1));
+		this.question.setLayout(new GridLayout(0, 1));
 		this.question.add(new JLabel("    " + question + "    ", JLabel.CENTER));
 	}
 
@@ -39,12 +39,12 @@ public class Question {
 	static void showResults() {
 		JOptionPane.showMessageDialog(null, nCorrect + " correct out of " + nQuestions + " questions.");
 	}
-	
+
 	void initQuestionDialog() {
 		this.question.setModal(true);
 		this.question.pack();
 		this.question.setLocationRelativeTo(null);
-		
+
 	}
-	
+
 }
